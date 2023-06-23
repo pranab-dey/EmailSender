1287f28161700f09ecde74c4959665f70f57da94
pranab-dey <pranab.strings07@gmail.com>
2023-06-24 01:07:36 +0600

    add command for test

--- src/tests/emailQueueProcessor.test.js
+++ src/tests/emailQueueProcessor.test.js
@@ -3,59 +3,53 @@ const proxyquire = require('proxyquire');
 const { expect } = require('chai');
 
 describe('emailQueueProcessor', () => {
-  let sendEmailStub, createCustomersStub, emailQueueProcessor;
+  let emailQueueProcessor;
+  let sendEmailStub;
+  let createCustomersStub;
 
   beforeEach(() => {
-    sendEmailStub = sinon.stub().resolves({ messageId: '12345' });
+    sendEmailStub = sinon.stub().resolves({ messageId: '123' });
     createCustomersStub = sinon.stub().resolves(true);
 
-    emailQueueProcessor = proxyquire('../emailQueueProcessor', {
-      '../helpers': { sendEmail: sendEmailStub },
-      '../services': { createCustomers: createCustomersStub },
+    emailQueueProcessor = proxyquire('../processors/emailQueueProcessor', {
+      '../helpers': {
+        sendEmail: sendEmailStub,
+      },
+      '../services': {
+        createCustomers: createCustomersStub,
+      },
     });
+  });
+
+  afterEach(() => {
+    sinon.restore();
   });
 
   it('should send emails and create customers in the database', async () => {
     const job = {
-      id: 'jobId',
+      id: 'job-id',
       data: [
         {
           enabled: 'true',
           to: 'test@example.com',
-          subject: 'Test Email',
-          body: 'This is a test email.',
+          subject: 'Test',
+          body: 'Hello',
         },
-      ],
-    };
-
-    const done = sinon.spy();
-
-    await emailQueueProcessor(job, done);
-
-    expect(sendEmailStub.calledOnce).to.be.true;
-    expect(createCustomersStub.calledOnce).to.be.true;
-    expect(done.calledOnce).to.be.true;
-  });
-
-  it('should handle errors and create customers in the database', async () => {
-    const job = {
-      id: 'jobId',
-      data: [
         {
-          enabled: 'false',
-          to: 'test@example.com',
-          subject: 'Test Email',
-          body: 'This is a test email.',
+          enabled: 'true',
+          to: 'another@example.com',
+          subject: 'Another Test',
+          body: 'Hi',
         },
       ],
     };
 
-    const done = sinon.spy();
+    const done = sinon.stub();
 
     await emailQueueProcessor(job, done);
 
-    expect(sendEmailStub.notCalled).to.be.true;
-    expect(createCustomersStub.calledOnce).to.be.true;
-    expect(done.calledOnce).to.be.true;
+    expect(sendEmailStub.callCount).to.equal(2);
+    expect(createCustomersStub.callCount).to.equal(1);
+    expect(done.callCount).to.equal(1);
   });
 });
